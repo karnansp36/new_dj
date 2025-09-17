86faca77523842916f265ff4ed0d4b9552a04484
karnansp36 <reviewmaster36@gmail.com>
2025-09-17 13:58:11 +0530

    first commit

---
+++ chat_pro/user_auth/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Signup(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
