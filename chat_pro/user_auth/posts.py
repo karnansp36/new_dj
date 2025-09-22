e13dd51d49e063dd424ccc16c13ce79463a5b3ae
karnansp36 <reviewmaster36@gmail.com>
2025-09-22 14:38:46 +0530

    day-2

---
+++ chat_pro/user_auth/posts.py
@@ -0,0 +1,3 @@
+from django.http import HttpResponse
+def hello():
+    return HttpResponse('hello')
--- chat_pro/user_auth/urls.py
+++ chat_pro/user_auth/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from . import posts
 urlpatterns =[
     path('home/', views.home),
     path('about/', views.about),
     path('signup/', views.signup),
+    path('hello/', posts.hello)
 ]
