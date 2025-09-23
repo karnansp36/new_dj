bb8af657793e7025d244ce59932264f8d6ec4478
karnansp36 <reviewmaster36@gmail.com>
2025-09-23 14:07:44 +0530

    day-4

--- chat_pro/user_log/urls.py
+++ chat_pro/user_log/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import user_log, login_view, profile_view
+from .views import user_log, login_view, profile_view, explore_profile, read_profile, profile_update
 
 urlpatterns =[
     path('signup/', user_log, name='signup'),
     path('login/', login_view, name='login'),
     path('profile/', profile_view, name='profile'),
+    path('explore/',explore_profile, name='exlore' ),
+    path('explore/profile/<int:id>', read_profile),
+    path('profile_update/<int:id>', profile_update, name='profile_update')
 ]
--- chat_pro/user_log/views.py
+++ chat_pro/user_log/views.py
@@ -21,7 +21,8 @@ def login_view(request):
 
         user = UserSignup.objects.filter(username=username, password=password)
         if user:
-            request.session['username'] = username
+            print(user[0].id)
+            request.session['id'] = user[0].id
             messages.success(request, "Login successful")
         else:
             # Here you would typically check the username and password against the database
@@ -29,15 +30,47 @@ def login_view(request):
     return render(request, 'log/login.html')
 
 def profile_view(request):
-    username = request.session.get('username')
+    username = request.session.get('id')
     if not username:
         messages.error(request, "You must be logged in to view the profile.")
         return redirect('login')
 
     try:
-        user = UserSignup.objects.get(username=username)
+        user = UserSignup.objects.get(id=username)
     except UserSignup.DoesNotExist:
         messages.error(request, "User does not exist.")
         return render(request, 'log/login.html')
 
     return render(request, 'log/profile.html', {"user": user})
+
+
+
+def profile_update(request, id):
+    # user_data = request.session.get('username')
+    # print(user_data, user.username)
+    # if user_data ==  user.username:
+    user = UserSignup.objects.get(id = id)
+    form = UserSignupForm(request.POST, request.FILES, instance=user)
+    if form.is_valid():
+        form.save()
+       
+    return render(request, 'log/signup.html', {"form" : UserSignupForm(instance=user)})
+
+
+
+
+
+
+
+def explore_profile(request):
+    users = UserSignup.objects.all()
+    if not users:
+        messages.error(request, 'profile is empty')
+
+    return render(request, 'explore.html', {'users':users})
+
+
+
+def read_profile(request, id):
+    user = UserSignup.objects.get(id = id)
+    return render(request, 'log/profile.html', {'user':user} )
