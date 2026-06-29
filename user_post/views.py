fb4519c8783e827c17cf6999070e4e586fc139ee
karnansp36 <reviewmaster36@gmail.com>
2026-06-29 15:22:15 +0530

    Add user_like app with initial setup and signup functionality

--- user_post/views.py
+++ user_post/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User_data
 # Create your views here.
 def signup(request):
-    pass
-
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        if username == "" or email == "" or password == "":
+            return HttpResponse("Please fill all the fields")
+        elif User_data.objects.filter(email=email).exists():
+            return HttpResponse("Email already exists")
+        elif len(password) < 8:
+            return HttpResponse("Password must be at least 8 characters long")
+        else:
+            user = User_data(name=username, email=email, password=password)
+            user.save()
+    return render(request, 'signup.html')
+# cross site request forgery
 
 def login(request):
     return render(request, 'index.html')
